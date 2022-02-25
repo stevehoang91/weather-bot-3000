0215991b6a62c2cfbc770e2b2fe79341278930da
TF <tf@tfs-imac.lan>
2022-02-25 08:07:51 +0000

    days of the week fix

--- config.js
+++ config.js
@@ -1,13 +1,14 @@
 export const apiKey = "5dd60cb12931e87d22344f75a802e824";
 
 export const weekday = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
 
- export const geoApiKey = "bcf7e6bbd41d402ea9474185da7f859f";
+export const geoApiKey = "bcf7e6bbd41d402ea9474185da7f859f";
